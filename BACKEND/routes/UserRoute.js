ba092f2423668be271b85045fc8658d6cc1eef4e
Yasindu Pasanjith <128167079+YASSA2001@users.noreply.github.com>
2025-03-13 17:44:31 +0530

    new

--- BACKEND/routes/UserRoute.js
+++ BACKEND/routes/UserRoute.js
@@ -1,5 +1,21 @@
-export const user = {
-    user_id: "1",
-    user_name: "John Doe",
-    user_email: "aKd0k@example.com",
-}
+import express from 'express';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    const { name, email, password, role } = req.body;
+    try {
+        const user = new User({ name, email, password, role });
+        await user.save();
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error registering user', error });
+    }
+});
+
+
+
+export default router;
