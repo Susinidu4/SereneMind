efa50e13d7fbb8bec68898afb7f84ce5a93174cd
WIJERATHNA A.D.S.S <susinidusachinthana@gmail.com>
2025-03-16 19:47:48 +0530

    Create header, footer, mood journaling pages

--- frontend/src/pages/Mood_Journaling/MoodJournalingInsert.jsx
+++ frontend/src/pages/Mood_Journaling/MoodJournalingInsert.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import GlobalStyle from "../../assets/Prototype/GlobalStyle";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
 
-export const Home = () => {
+export const MoodJournalingInsert = () => {
   return (
     <div className="flex flex-col min-h-screen bg-[#FFFDF7]">
       <Header />
 
       <main className="flex-grow mx-20">
         <div className={GlobalStyle.fontNunito}>
-          {/* Your main content goes here */}
+          <div className={`${GlobalStyle.pageContainer} px-25`}>
+            
+          </div>
         </div>
       </main>
-            
+
       <Footer />
     </div>
   );
