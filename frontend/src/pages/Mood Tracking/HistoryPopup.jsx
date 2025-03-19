ca84ca8c902e9179e94b707eae2bfd8117566c45
Yasindu Pasanjith <128167079+YASSA2001@users.noreply.github.com>
2025-03-19 12:58:14 +0530

    update calendar

--- frontend/src/pages/Mood Tracking/HistoryPopup.jsx
+++ frontend/src/pages/Mood Tracking/HistoryPopup.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import "./Calendar.css";
+import { IoCloseCircle } from "react-icons/io5";
 
 export const HistoryPopup = ({ selectedDate, loading, error, filteredMoodHistory, onClose }) => {
+
   return (
     <div className="popup-overlay">
       <div className="popup">
         <div className="popup-content">
+          <div className="close-btn float-right" onClick={onClose}>
+          <IoCloseCircle size={25} color="red" />
+          </div>
           <h3>Mood History for {selectedDate}</h3>
-          <button onClick={onClose}>Close</button>
           {loading ? (
             <p>Loading...</p>
           ) : error ? (
