4c89033f633422c53772f3a2c808104026c17ba4
Yasindu Pasanjith <128167079+YASSA2001@users.noreply.github.com>
2025-03-23 13:31:02 +0530

    update sign up and sign in pages

--- frontend/src/pages/Main_Pages/User Account Management/User_Login.jsx
+++ frontend/src/pages/Main_Pages/User Account Management/User_Login.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { Header } from '../../../components/Header';
-
+import GlobalStyle from '../../../assets/Prototype/GlobalStyle';
+import { Link } from 'react-router-dom';
 export const User_Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(''); // State to handle errors
 
-  const user = JSON.parse(localStorage.getItem('userData'));
-  if (user?.role === 'user' || user?.role === 'User' || user?.role === 'USER') {
-    window.location.href = '/userprofile';
-  }
+  // const user = JSON.parse(localStorage.getItem('userData'));
+  // if (user?.role === 'user' || user?.role === 'User' || user?.role === 'USER') {
+  //   window.location.href = '/userprofile';
+  // }
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -57,11 +58,11 @@ export const User_Login = () => {
   };
 
   return (
-    <div>
+    <div className="flex flex-col min-h-screen bg-[#FFFDF7]" style={{fontFamily:"'Nunito"}}>
       <Header />
-      <div className='flex justify-center items-center mt-10'>
-        <div className='bg-white p-8 rounded-lg shadow-md w-full max-w-md'>
-          <h1 className='text-3xl font-bold text-center mb-6'>Login Page</h1>
+      <main className="flex mx-20 justify-center items-center">
+      <div className={`bg-[#E9F1F1] p-8 rounded-lg shadow-lg w-full max-w-md`}>
+          <h1 className={`font-bold text-2xl text-center`}>Login</h1>
           {error && <p className='text-red-500 text-center mb-4'>{error}</p>}
           <form onSubmit={handleLogin}>
             <div className='mb-4'>
@@ -72,7 +73,7 @@ export const User_Login = () => {
                 name="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                className='mt-1 bg-[#FFFDF7] block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                 required
               />
             </div>
@@ -84,19 +85,23 @@ export const User_Login = () => {
                 name="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-                className='mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
+                className='mt-1 bg-[#FFFDF7] block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
                 required
               />
             </div>
             <button
               type="submit"
-              className='w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
+              className='w-full bg-[#92C9B1] text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
             >
               Login
             </button>
+
+           <Link to={`/usersignup`}>
+           <p className="text-center mt-4 font-bold">Don't have an account? <span className="text-[#92C9B1]">Sign Up</span></p>
+           </Link>
           </form>
         </div>
-      </div>
+      </main>
     </div>
   );
 };
